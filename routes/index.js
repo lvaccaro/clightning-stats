265c84a06b704364b9804a1c2cae3362ecdd9272
Luca Vaccaro <luck87@gmail.com>
2018-02-10 02:25:56 +0100

    1st upload

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const LightningClient = require('lightning-client');
+const client = new LightningClient('/root/.lightning', true);
+
+/* GET home page. */
+router.get('/getinfo', function(req, res, next) {
+    client.getinfo()
+        .then(info => {res.status(200).send(info);})
+        .catch(err => {res.status(500);});
+});
+
+router.get('/listpeers', function(req, res, next) {
+    client.listpeers()
+        .then(peers => {res.status(200).send(peers);})
+        .catch(err => {res.status(500);});
+});
+
+router.get('/listnodes', function(req, res, next) {
+    client.listnodes()
+        .then(nodes => {res.status(200).send(nodes);})
+.catch(err => {res.status(500);});
+});
+
+module.exports = router;
