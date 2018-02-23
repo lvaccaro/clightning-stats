5fb5206f7eb8fd137c2c5e17c817746660e6c990
Luca Vaccaro <luck87@gmail.com>
2018-02-24 00:30:18 +0100

    Add process env for lightning dir

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const LightningClient = require('lightning-client');
-const client = new LightningClient('/root/.lightning', true);
+const client = new LightningClient(process.env.LIGHTNINGDIR || '/root/.lightning', true);
 
 /* GET home page. */
 router.get('/getinfo', function(req, res, next) {
