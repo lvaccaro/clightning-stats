b3696071a04d35178846663050473eb2b3674cb9
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-05-17 19:25:57 +0200

    config: restored original url

--- public/index.js
+++ public/index.js
@@ -3,8 +3,7 @@
 /* global XMLHttpRequest */
 
 // Configs
-// const url = 'http://btctest.waldo.fun:4000'; // Your server url
-const url = 'http://127.0.0.1:4000';
+const url = 'http://btctest.waldo.fun:4000'; // Your server url
 
 // Init calls
 printUrl(url);
