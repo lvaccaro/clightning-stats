3bdc1d29930a91294c3116926813431ade9c512e
Luca Vaccaro <luck87@gmail.com>
2018-06-28 00:32:13 +0200

    Fix xo lint

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', index);
 app.use((req, res, next) => {
 	const err = new Error('Not Found');
 	err.status = 404;
-  next(err);
+	next(err);
 });
 
 // Error handler
@@ -38,9 +38,9 @@ app.use((err, req, res, /* next */) => {
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// Render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
